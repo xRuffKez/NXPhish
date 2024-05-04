bd9ad303bf867d09d11f10874e36ca45c395a566
xRuffKez <92758042+xRuffKez@users.noreply.github.com>
2024-05-05 00:09:08 +0200

    Update stats.py

--- py/stats.py
+++ py/stats.py
@@ -26,26 +26,30 @@ daily_counts_servfail = df[df['status'] == 'SERVFAIL'].groupby(df['last_seen'].d
 # Get top 10 abused TLDs with DNS status OK
 top_tlds = df[df['status'] == 'OK']['domain'].apply(lambda x: x.split('.')[-1]).value_counts().head(10)
 
-# Create the figure
+# Create the figure with subplots
 fig = go.Figure()
 
-# Add traces for each status
+# Add traces for main graph (OK, NXDOMAIN, SERVFAIL)
 fig.add_trace(go.Scatter(x=daily_counts_ok.index[::-1], y=daily_counts_ok.values[::-1], mode='lines+markers', name='OK'))
 fig.add_trace(go.Scatter(x=daily_counts_nxdomain.index[::-1], y=daily_counts_nxdomain.values[::-1], mode='lines+markers', name='NXDOMAIN'))
 fig.add_trace(go.Scatter(x=daily_counts_servfail.index[::-1], y=daily_counts_servfail.values[::-1], mode='lines+markers', name='SERVFAIL'))
 
-# Add traces for top 10 abused TLDs with DNS status OK
-for tld in top_tlds.index:
-    tld_counts = df[(df['status'] == 'OK') & (df['domain'].str.endswith(tld))].groupby(df['last_seen'].dt.date).size()
-    fig.add_trace(go.Scatter(x=tld_counts.index[::-1], y=tld_counts.values[::-1], mode='lines+markers', name=tld, line=dict(width=1)))  # Adjust line thickness
-
-# Update layout
+# Update layout for main graph
 fig.update_layout(title='Daily Phishing Domain Counts (Last 60 Days)',
                   xaxis_title='Date',
                   yaxis_title='Count',
                   xaxis=dict(tickangle=-45),
                   xaxis_range=[start_date, datetime.now()],  # Adjust x-axis range
                   template='plotly_white')
+
+# Create a subplot for top 10 abused TLDs
+fig.add_trace(go.Bar(x=top_tlds.index[::-1], y=top_tlds.values[::-1], name='TLDs', marker_color='lightsalmon'))
+
+# Update layout for subplot
+fig.update_layout(yaxis2_title='TLD Count',  # Y-axis title for subplot
+                  yaxis2=dict(anchor='x2', overlaying='y', side='right'),  # Position and overlaying of y-axis
+                  template='plotly_white',
+                  height=800)  # Adjust height of the plot
 
 # Save the graph as an image
 os.makedirs('stor', exist_ok=True)
