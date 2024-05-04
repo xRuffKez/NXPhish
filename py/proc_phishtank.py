f59cd8a1babc07e16d90bccd1b4824384bdce987
xRuffKez <92758042+xRuffKez@users.noreply.github.com>
2024-05-04 20:49:55 +0200

    Update proc_phishtank.py

--- py/proc_phishtank.py
+++ py/proc_phishtank.py
@@ -14,7 +14,7 @@ def check_dns_status(domain):
         return "OK"
     except dns.resolver.NXDOMAIN:
         return "NXDOMAIN"
-    except dns.resolver.NoAnswer:
+    except (dns.resolver.NoAnswer, dns.resolver.SERVFAIL):
         return "SERVFAIL"
     except dns.resolver.REFUSED:
         return "REFUSED"
