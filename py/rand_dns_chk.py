585c9e95e2d767168387b42701c65996e5fa7533
xRuffKez <92758042+xRuffKez@users.noreply.github.com>
2024-05-04 19:17:58 +0200

    Update rand_dns_chk.py

--- py/rand_dns_chk.py
+++ py/rand_dns_chk.py
@@ -20,7 +20,6 @@ def update_dns_status():
     db_path = "stor/cache.db"
     max_age = datetime.now() - timedelta(days=60)
 
-    # Open database connection
     with sqlite3.connect(db_path) as conn:
         cursor = conn.cursor()
         cursor.execute("SELECT domain, status FROM domains WHERE status NOT IN (?, ?) ORDER BY RANDOM() LIMIT 5000",
@@ -29,15 +28,12 @@ def update_dns_status():
 
     with ThreadPoolExecutor(max_workers=4) as executor:
         results = executor.map(resolve_domain, [domain for domain, _ in domains_to_check])
-    
-    # Open database connection again for updating
+
     with sqlite3.connect(db_path) as conn:
         cursor = conn.cursor()
         update_values = [(result, domain) for (domain, _), result in zip(domains_to_check, results) if result is not None]
         cursor.executemany("UPDATE domains SET status=? WHERE domain=?", update_values)
         conn.commit()
-        
-    conn.close()
 
 if __name__ == "__main__":
     update_dns_status()
