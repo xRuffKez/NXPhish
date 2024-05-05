6fecb721f2df1a988df7a3057e81f8f79c577962
xRuffKez <92758042+xRuffKez@users.noreply.github.com>
2024-05-05 10:48:24 +0200

    Create debug.py

---
+++ py/debug.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+def set_servfail_domains_to_ok():
+    db_path = "stor/cache.db"
+    with sqlite3.connect(db_path) as conn:
+        cursor = conn.cursor()
+        cursor.execute("SELECT domain FROM domains WHERE status = 'SERVFAIL'")
+        servfail_domains = cursor.fetchall()
+
+        if servfail_domains:
+            updated_domains = [(domain[0], "OK") for domain in servfail_domains]
+            cursor.executemany("UPDATE domains SET status=? WHERE domain=?", updated_domains)
+            conn.commit()
+            print("Updated", len(updated_domains), "domains from SERVFAIL to OK.")
+        else:
+            print("No domains with SERVFAIL status found.")
+
+if __name__ == "__main__":
+    set_servfail_domains_to_ok()
