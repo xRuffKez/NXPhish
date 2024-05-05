0f05d9497b56584431a9d0fdce51aa61d3231d5e
xRuffKez <92758042+xRuffKez@users.noreply.github.com>
2024-05-05 10:17:15 +0200

    Update proc_list_output.py

--- py/proc_list_output.py
+++ py/proc_list_output.py
@@ -76,7 +76,7 @@ def update_phishfeed(workspace):
                                 status = "OK"
                             except dns.resolver.NXDOMAIN:
                                 status = "NXDOMAIN"
-                            except dns.resolver.NoAnswer:
+                            except (dns.resolver.NoAnswer, dns.resolver.Timeout):
                                 status = "SERVFAIL"
                             except Exception as e:
                                 logger.error("Error resolving domain %s: %s", domain, e)
