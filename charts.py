9d08a79ba13bca689a4bf56fe915a1e04413fa49
xRuffKez <92758042+xRuffKez@users.noreply.github.com>
2024-05-09 19:01:57 +0200

    Update charts.py

--- charts.py
+++ charts.py
@@ -1,11 +1,12 @@
 import json
 from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
+import os
 
 with open('warehouse.json', 'r') as file:
     data = json.load(file)
 
-phishing_domains = [entry for entry in data if entry['dns_status'] == 'OK' and entry['whitelisted'] == '0']
+phishing_domains = [entry for entry in data if entry['dns_status'] == 'OK' and entry['domain_type'] == 'Phishing']
 
 now = datetime.now()
 twenty_four_hours_ago = now - timedelta(hours=24)
@@ -28,7 +29,8 @@ def plot_chart_last_24_hours(domains_data, title):
     plt.ylabel('Number of Phishing Domains')
     plt.xticks(rotation=45)
     plt.tight_layout()
-    plt.show()
+    plt.savefig('phishing_domains_last_24_hours.png')  # Save the plot as a PNG file
+    plt.close()
 
 def plot_chart_7_days(domains_data, title):
     dates = [datetime.fromisoformat(entry['date']) for entry in domains_data]
@@ -40,7 +42,8 @@ def plot_chart_7_days(domains_data, title):
     plt.ylabel('Number of Phishing Domains')
     plt.xticks(rotation=45)
     plt.tight_layout()
-    plt.show()
+    plt.savefig('phishing_domains_last_7_days.png')  # Save the plot as a PNG file
+    plt.close()
 
 def plot_chart_1_month(domains_data, title):
     dates = [datetime.fromisoformat(entry['date']) for entry in domains_data]
@@ -52,7 +55,8 @@ def plot_chart_1_month(domains_data, title):
     plt.ylabel('Number of Phishing Domains')
     plt.xticks(rotation=45)
     plt.tight_layout()
-    plt.show()
+    plt.savefig('phishing_domains_last_month.png')  # Save the plot as a PNG file
+    plt.close()
 
 def plot_chart_1_year(domains_data, title):
     dates = [datetime.fromisoformat(entry['date']) for entry in domains_data]
@@ -64,9 +68,17 @@ def plot_chart_1_year(domains_data, title):
     plt.ylabel('Number of Phishing Domains')
     plt.xticks(rotation=45)
     plt.tight_layout()
-    plt.show()
+    plt.savefig('phishing_domains_last_year.png')  # Save the plot as a PNG file
+    plt.close()
 
 plot_chart_last_24_hours(phishing_domains_last_24_hours, 'Phishing Domains with DNS Status "OK" - Last 24 Hours')
 plot_chart_7_days(phishing_domains_last_week, 'Phishing Domains with DNS Status "OK" - Last 7 Days')
 plot_chart_1_month(phishing_domains_last_month, 'Phishing Domains with DNS Status "OK" - Last Month')
 plot_chart_1_year(phishing_domains_last_year, 'Phishing Domains with DNS Status "OK" - Last Year')
+
+if not os.path.exists('generated_charts'):
+    os.makedirs('generated_charts')
+
+for filename in ['phishing_domains_last_24_hours.png', 'phishing_domains_last_7_days.png', 'phishing_domains_last_month.png', 'phishing_domains_last_year.png']:
+    if os.path.exists(filename):
+        os.rename(filename, os.path.join('generated_charts', filename))
