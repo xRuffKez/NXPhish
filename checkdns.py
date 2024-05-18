89d1df1aef90026e4040148445161c695a91eb3c
xRuffKez <92758042+xRuffKez@users.noreply.github.com>
2024-05-18 06:56:39 +0200

    Update checkdns.py

--- checkdns.py
+++ checkdns.py
@@ -2,6 +2,7 @@ import json
 import time
 import dns.resolver
 
+# Function to check DNS status of a given domain
 def check_dns_status(domain):
     resolver = dns.resolver.Resolver()
     resolver.nameservers = ['76.76.2.0', '76.76.10.0']
@@ -12,11 +13,7 @@ def check_dns_status(domain):
         ipv6_response = resolver.resolve(domain, 'AAAA')
         ipv6_addresses = [r.address for r in ipv6_response]
 
-        if ipv4_addresses and ipv6_addresses:
-            return "OK"
-        elif ipv4_addresses:
-            return "OK"
-        elif ipv6_addresses:
+        if ipv4_addresses or ipv6_addresses:
             return "OK"
         else:
             return "NO_ANSWER"
@@ -28,29 +25,31 @@ def check_dns_status(domain):
         return "NO_ANSWER"
     except dns.resolver.NoNameservers:
         return "NO_NAMESERVERS"
-    except dns.resolver.NoRootSOA:
-        return "NO_ROOT_SOA"
-    except dns.resolver.NoRootNS:
-        return "NO_ROOT_NS"
-    except dns.resolver.NoMetaqueries:
-        return "NO_METAQUERIES"
-    except dns.resolver.NoMetaqueriesNoRootNS:
-        return "NO_METAQUERIES_NO_ROOT_NS"
     except Exception as e:
         return "ERROR"
 
+# Function to update DNS status in warehouse.json
 def update_dns_status():
     current_time = int(time.time())
-    with open("warehouse.json", "r+") as file:
-        data = json.load(file)
-        for entry in data:
-            if entry["dns_check_date"] == 0 or current_time - entry["dns_check_date"] >= 48 * 3600:
-                dns_status = check_dns_status(entry["domain"])
-                entry["dns_status"] = dns_status
-                entry["dns_check_date"] = current_time
-        file.seek(0)
-        file.truncate()
-        json.dump(data, file, indent=4)
+    try:
+        with open("warehouse.json", "r+") as file:
+            data = json.load(file)
+
+            for entry in data:
+                if entry["dns_check_date"] == 0 or current_time - entry["dns_check_date"] >= 48 * 3600:
+                    entry["dns_status"] = check_dns_status(entry["domain"])
+                    entry["dns_check_date"] = current_time
+
+            file.seek(0)
+            file.truncate()
+            json.dump(data, file, indent=4)
+        print("DNS status updated successfully.")
+    except FileNotFoundError:
+        print("Error: 'warehouse.json' not found.")
+    except json.JSONDecodeError:
+        print("Error: Failed to decode JSON from 'warehouse.json'.")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
 
 if __name__ == "__main__":
     update_dns_status()
